72f18fb5143ab60d674fecc2909a658f3c49bc5a
Debajoti Chakraborty <chakrabortydebajoti@gmail.com>
2024-11-27 15:23:06 +0530

    Fix Deployment 0.04

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,43 @@
 "use client";
 import { buttonVariants } from "@/components/ui/button";
-import { ArrowRight} from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 import { DNA } from "react-loader-spinner";
 
 export default function Home() {
   return (
-    <div className="text-center mx-auto px-20">
+    <div className="text-center mx-auto px-6 sm:px-20 w-full max-w-screen-lg">
       <div className="flex justify-center">
         <DNA
           visible={true}
-          height="200"
-          width="200"
+          height="150"
+          width="150" 
           ariaLabel="dna-loading"
           wrapperStyle={{}}
           wrapperClass="dna-wrapper"
         />
       </div>
-      <div className="text-8xl font-bold">
+      <div className="text-4xl sm:text-8xl font-bold">
         <span className="relative font-bold text-3xl text-white">
           <span
-            className="absolute left-1/2 -translate-x-1/2 -translate-y-0 bottom-0 w-full h-6 bg-gradient-to-r from-teal-700 to-blue-400"
+            className="absolute left-1/2 -translate-x-1/2 -translate-y-0 bottom-0 w-full h-4 sm:h-6 bg-gradient-to-r from-teal-700 to-blue-400"
             aria-hidden="true"
           ></span>
-          <span className="z-2 relative text-8xl">Accessor</span>
+          <span className="z-2 relative text-6xl sm:text-8xl">Accessor</span>
         </span>
       </div>
-      <div className="sm:w-1/2 mx-auto mt-5">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat,
-        perspiciatis beatae tempora molestiae incidunt, quisquam, laudantium
-        dolorem delectus pariatur nostrum maxime! Recusandae itaque voluptatibus
-        culpa optio tenetur? Dolorum, delectus consequuntur. Molestias
-        repudiandae ipsa quidem dolorum sit aperiam error odio, molestiae dolor
-        alias consequuntur aliquid sunt architecto doloremque ullam laboriosam
-        eos ab eum possimus, neque nihil accusamus ea consectetur harum? Animi
-        ullam alias officia delectus, inventore veniam, at eos ipsam vitae
-        architecto mollitia nisi.
+      <div className="sm:w-1/2 mx-auto mt-5 text-sm sm:text-base">
+      Accessor is a cutting-edge platform designed to help individuals and
+          organizations manage their assets and resources efficiently. Whether you're
+          looking to track personal items, manage business equipment, or streamline
+          inventory management, Accessor provides a user-friendly interface to keep
+          your valuable assets organized and easily accessible. With powerful features like real-time tracking, asset categorization,
+          and secure sharing options, Accessor simplifies the management of physical
+          and digital assets.
       </div>
       <div className="mt-5">
-        <Link className={buttonVariants()} href="/log-in">
-          {" "}
-          Get Started {<ArrowRight />}
+        <Link className={`${buttonVariants()} px-6 py-2 text-base sm:text-lg`} href="/log-in">
+          {" "} Get Started {<ArrowRight />}
         </Link>
       </div>
     </div>
